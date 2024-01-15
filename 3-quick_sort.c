a44a3e712b5a2614f559913dc6fb5e26b58505c9
Miniknacky <jonathanhuybrechts@gmail.com>
2024-01-15 21:27:29 +0000

    test task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,71 +1,57 @@
 #include "sort.h"
 
 /**
- * lomuto_partition - Lomuto partition scheme for quicksort.
- * @array: The array to sort.
- * @low: The lowest index of the sub-array to partition.
- * @high: The highest index of the sub-array to partition.
- * @size: The size of the array.
- *
- * Return: The final partition index.
- */
-int lomuto_partition(int *array, int low, int high, size_t size)
-{
-	int pivot = array[high];
-	int i = low - 1;
-
-	for (int j = low; j < high; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			if (i != j)
-			{
-				int temp = array[i];
-
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
-		}
-	}
-	int temp = array[i + 1];
-
-	array[i + 1] = array[high];
-	array[high] = temp;
-	if (i + 1 != high)
-		print_array(array, size);
-	return (i + 1);
-}
-
-/**
- * quick_sort_helper - Helper function to perform quicksort recursively.
- * @array: The array to sort.
- * @low: The lowest index of the sub-array to sort.
- * @high: The highest index of the sub-array to sort.
- * @size: The size of the array.
- */
-void quick_sort_helper(int *array, int low, int high, size_t size)
-{
-	if (low < high)
-	{
-		int pi = lomuto_partition(array, low, high, size);
-
-		quick_sort_helper(array, low, pi - 1, size);
-		quick_sort_helper(array, pi + 1, high, size);
-	}
-}
-
-/**
  * quick_sort - Sorts an array of integers in ascending order using
- * the Quick Sort algorithm.
+ *              the Quick Sort algorithm.
  * @array: The array of integers to be sorted.
  * @size: The size of the array.
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
-	quick_sort_helper(array, 0, size - 1, size);
+	quick_sort_recursive(array, 0, size - 1, size);
+}
+
+/**
+ * quick_sort_recursive - Helper function to perform quicksort recursively.
+ * @array: The array to sort.
+ * @low: The lowest index of the sub-array to sort.
+ * @high: The highest index of the sub-array to sort.
+ * @size: The size of the array.
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot = array[high];
+		int i = low - 1;
+
+		for (int j = low; j < high; j++)
+		{
+			if (array[j] <= pivot)
+			{
+				i++;
+				if (i != j)
+				{
+					int temp = array[i];
+
+					array[i] = array[j];
+					array[j] = temp;
+					print_array(array, size);
+				}
+			}
+		}
+
+		int temp = array[i + 1];
+
+		array[i + 1] = array[high];
+		array[high] = temp;
+		if (i + 1 != high)
+			print_array(array, size);
+
+		quick_sort_recursive(array, low, i, size);
+		quick_sort_recursive(array, i + 2, high, size);
+	}
 }
