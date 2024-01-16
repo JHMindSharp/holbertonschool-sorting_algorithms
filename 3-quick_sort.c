e78b6b9ec9ca50ad035f462f6108505a8157c0bd
Miniknacky <jonathanhuybrechts@gmail.com>
2024-01-16 10:07:13 +0000

    test file big O 0

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,57 +1,67 @@
 #include "sort.h"
 
 /**
- * quick_sort - Sorts an array of integers in ascending order using
- *              the Quick Sort algorithm.
+ * quick_sort - Sorts an array of integers in ascending order
+ * using the Quick sort algorithm.
  * @array: The array of integers to be sorted.
  * @size: The size of the array.
+ *
+ * Description: This function sorts an array of integers in ascending order
+ * using the Quick Sort algorithm. It implements the Lomuto partition scheme
+ * with the pivot as the last element of the partition being sorted.
+ * It prints the array after each swap of two elements.
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2)
+	if (!array || size < 2)
 		return;
 
-	quick_sort_recursive(array, 0, size - 1, size);
-}
+	int stack[size];
+	int top = -1;
+	int i, j;
 
-/**
- * quick_sort_recursive - Helper function to perform quicksort recursively.
- * @array: The array to sort.
- * @low: The lowest index of the sub-array to sort.
- * @high: The highest index of the sub-array to sort.
- * @size: The size of the array.
- */
-void quick_sort_recursive(int *array, int low, int high, size_t size)
-{
-	if (low < high)
+	stack[++top] = 0;
+	stack[++top] = size - 1;
+
+	while (top >= 0)
 	{
+		int high = stack[top--];
+		int low = stack[top--];
 		int pivot = array[high];
-		int i = low - 1;
+		i = low;
 
-		for (int j = low; j < high; j++)
+		for (j = low; j < high; j++)
 		{
 			if (array[j] <= pivot)
 			{
-				i++;
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
 				if (i != j)
 				{
-					int temp = array[i];
-
-					array[i] = array[j];
-					array[j] = temp;
 					print_array(array, size);
 				}
+				i++;
 			}
 		}
 
-		int temp = array[i + 1];
-
-		array[i + 1] = array[high];
+		int temp = array[i];
+		array[i] = array[high];
 		array[high] = temp;
-		if (i + 1 != high)
+		if (i != high)
+		{
 			print_array(array, size);
+		}
 
-		quick_sort_recursive(array, low, i, size);
-		quick_sort_recursive(array, i + 2, high, size);
+		if (low < i - 1)
+		{
+			stack[++top] = low;
+			stack[++top] = i - 1;
+		}
+		if (i + 1 < high)
+		{
+			stack[++top] = i + 1;
+			stack[++top] = high;
+		}
 	}
 }
--- sort.h
+++ sort.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stddef.h>
 /* Structure for Doubly Linked List */
 /**
  * struct listint_s - Doubly linked list node
